bf6288cd45b95ca85227b1742d4146cf9e5f2fcb
ANA CLARA DE MELO SANTOS <11321949464@imaccommand023.local>
2025-11-18 14:36:58 -0300

    Exercicio 05

--- Exercicio05.js
+++ Exercicio05.js
@@ -3,9 +3,9 @@
 // crescente.Exibindo uma mensagem indicando se os números estão em ordem crescente ou
 // não.
 
-let numero01 = Number.parseFloat(prompt("Digite o primeiro número: "))
-let numero02 = Number.parseFloat(prompt("Digite o segundo número: "))
-let numero03 = Number.parseFloat(prompt("Digite o terceiro número: "))
+let numero01 = parseInt(prompt("Digite o primeiro número: "))
+let numero02 = parseInt(prompt("Digite o segundo número: "))
+let numero03 = parseInt(prompt("Digite o terceiro número: "))
 
 if (numero01 < numero02 && numero02 < numero03) {
     console.log("Os números estão em ordem crescente", numero01, numero02, numero03)
