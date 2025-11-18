a2bd259cba5388a92c85d15328fa166282eaa75b
ANA CLARA DE MELO SANTOS <11321949464@imaccommand023.local>
2025-11-18 11:29:48 -0300

    Exercicio 02

---
+++ Exercicio02.js
@@ -0,0 +1,46 @@
+// Exercício 2: Verificação de dia da semana
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia.Considere que 1 representa segunda - feira,
+//     2 representa terça - feira e assim por diante.
+
+
+let semana = window.prompt("Digite um número de 1 a 7")
+switch (semana) {
+    case "1":
+        console.log("segunda-feira")
+
+        break;
+
+    case "2":
+        console.log("terça-feira")
+
+        break;
+
+    case "3":
+        console.log("quarta-feira")
+
+        break;
+
+    case "4":
+        console.log("quinta-feira")
+
+        break;
+
+    case "5":
+        console.log("sexta-feira")
+
+        break;
+
+    case "6":
+        console.log("sábado")
+
+        break;
+
+    case "7":
+        console.log("domingo")
+
+        break;
+    default:
+        console.log("Dia inexistente")
+        break;
+}
