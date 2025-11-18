d93140c4ebe70ea891cf5ee87252f6d88edeea6b
ANA CLARA DE MELO SANTOS <11321949464@imaccommand023.local>
2025-11-18 10:46:54 -0300

    Exercicio 01

---
+++ montessa.js
@@ -0,0 +1,59 @@
+//socorro Deus
+//Alert- Apresenta uma mensagem na tela
+
+// window.alert("Oi, amigos!")
+
+// //Confirm- Pergunta com ok/cancela( Retorna um V ou F)
+// window.confirm("O Jonas vai casar!")
+
+// //Prompt- Pega o texto da tela
+// window.prompt("Qual é o seu nome?")
+
+// comentario apenas nessa linha
+/* 
+Comentario em texto, podendo fazer textos em varias linhas.
+*/
+
+// console.log("Multimidia")
+// // serve para recados, aparece em console no site
+
+// let nome = prompt("Informe o seu nome: ")
+
+// console.log("Bem vindo!" + nome) 
+
+// let numero1 =  Number.parseInt(prompt( "Digite o primeiro número: "))
+// let numero2 =  Number.parseInt (prompt (" Digite o segundo número: "))
+
+// let resultado = numero1 + numero2
+
+// let numero1 =  Number.parseFloat(prompt( "Digite o primeiro número: "))
+// let numero2 =  Number.parseFloat(prompt (" Digite o segundo número: "))
+
+// let resultado = numero1 + numero2
+
+
+// console.log(resultado)
+
+
+// let idade = window.prompt("Qual sua idade?")
+
+// if (idade >= 18) {
+//     console.log(" Maior de iade!!!")
+// } else {
+//     console.log(" Você é menor de idade!!")
+// }
+
+// switch (idade) {
+//     case "18":
+//         console.log("Maior de idade!!")
+
+//         break;
+
+//     case "27":
+//         console.log("Esta perto dos 30")
+//         break;
+
+//     default:
+//         console.log("Nada a dizer")
+//         break;
+// }
