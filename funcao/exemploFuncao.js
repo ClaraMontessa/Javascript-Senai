e4f0b0a6296f4e99689b198e84a906352683226d
Clara Melo <claramontessa@gmail.com>
2025-11-25 16:58:51 -0300

    Add files via upload

---
+++ funcao/exemploFuncao.js
@@ -0,0 +1,25 @@
+// function saudacao() {
+//     let nomePessoa = prompt("Digite seu nome: ")
+//     let horaAtual = new Date().getHours()
+
+//     if (horaAtual > 6 && horaAtual < 13) {
+//         alert("Bom dia " + nomePessoa)
+//     } else if (horaAtual >= 13 && horaAtual < 18) {
+//         alert("Boa tarde " + nomePessoa)
+//     } else {
+//         alert("Boa noite " + nomePessoa)
+//     }
+// }
+
+// saudacao()
+
+
+let pessoa = {
+    nome: "Ana Paula",
+    idade: 17,
+    sobrenome: "Nascimento"
+}
+
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(pessoa.sobrenome)
