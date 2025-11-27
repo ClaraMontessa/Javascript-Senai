56889b3f1b884085d5056e87f1e022721d42eef0
Clara Melo <claramontessa@gmail.com>
2025-11-27 16:49:39 -0300

    Add files via upload

--- DOM/dom.js
+++ DOM/dom.js
@@ -10,5 +10,7 @@ console.log(elementosPorClass[0]); //Array
 console.log(elementoPorID);
 console.log(elementoPorTag);
 
-elementoPorTag[0].textContent = "Verdades secretas";
+elementoPorTag[0].textContent = "Verdades secretas"; // Modifica o conteúdo  do elemento.
+elementoPorTag[0].style.color = "red"; 
+elementoPorTag[0].style.fontSize = "30px"; 
 console.log(elementoPorTag[0].textContent);
